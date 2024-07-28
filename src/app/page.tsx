5d88ee62b7133969d107cbaf05e7e8602213e116
Harsh Kumar <107759338+hrshkr09@users.noreply.github.com>
2024-07-28 10:15:43 +0530

    Add files via upload

---
+++ src/app/page.tsx
@@ -0,0 +1,24 @@
+import FeaturedCourses from "@/components/FeaturedCourses";
+import Footer from "@/components/Footer";
+import HeroSection from "@/components/HeroSection";
+import Instructors from "@/components/Instructors";
+import Testimonials from "@/components/TestimonialCards";
+import UpcomingWebinars from "@/components/UpcomingWebinars";
+import WhyChooseUs from "@/components/WhyChooseUs";
+import Image from "next/image";
+
+
+export default function Home() {
+  return (
+    <main className="min-h-screen text-white bg-black/[0.96] antialiased bg-grid-white/[0.02]">
+
+     <HeroSection/>
+     <FeaturedCourses/>
+     <WhyChooseUs/>
+     <Testimonials/>
+     <UpcomingWebinars/>
+     <Instructors/>
+     <Footer/>
+    </main>
+  );
+}
